f8545cfb9101590685d8549bfa6adb4233e4950f
Tejashri1128 <tejudatir@gmail.com>
2025-02-25 12:59:47 +0530

    redirect to login if not Authenticated & subscribe to store and set user according to token while refresh

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,36 @@
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import { BASE_URL } from "../utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user);
+  const fetchUser = async () => {
+    try {
+      if (userData) {
+        return;
+      }
+      const result = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(result.data));
+    } catch (error) {
+      if (error.response.status === 401) {
+        navigate("/login");
+      }
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
