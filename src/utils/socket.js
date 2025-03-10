3a5abbf1f3f4e8a1d1508c655a799bc823afc2a1
Tejashri1128 <tejudatir@gmail.com>
2025-03-11 01:44:13 +0530

    socket change for production

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,8 +2,11 @@ import { io } from "socket.io-client";
 import { BASE_URL } from "./constant";
 
 export const createSocketConnection = () => {
-  const socket = io(BASE_URL, {
-    withCredentials: true,
-  });
-  return socket;
+  if (location.hostname === "localhost") {
+    return io(BASE_URL);
+  } else {
+    return io("/", {
+      path: "/api/socket.io",
+    });
+  }
 };
